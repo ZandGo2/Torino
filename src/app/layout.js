02e8a0f0bb01a39ef7947c2cef3683da133a13bb
Mobin Zandkarimi <zandgo2@gmail.com>
2025-07-20 15:37:16 +0330

    make ui footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
       <body>
         <Header />
         {children}
-        {/* <Footer /> */}
+        <Footer />
       </body>
     </html>
   );
--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,71 +3,73 @@ import styles from "./layout.module.css";
 
 const Footer = () => {
   return (
-    <div>
-      <div>
-        <div>
-          <p>تورینو</p>
-          <ul>
-            <li>درباره ما</li>
-            <li>تماس با ما</li>
-            <li>چرا تورینو</li>
-            <li>بیمه مسافرتی</li>
-          </ul>
+    <div className={styles.FooterDiv}>
+      <div className={styles.MainFooter}>
+        <div className={styles.DivFirstListFooter}>
+          <div className={styles.FirstListFooter}>
+            <p>تورینو</p>
+            <ul>
+              <li>درباره ما</li>
+              <li>تماس با ما</li>
+              <li>چرا تورینو</li>
+              <li>بیمه مسافرتی</li>
+            </ul>
+          </div>
+          <div className={styles.FirstListFooter}>
+            <p>خدمات مشتریان</p>
+            <ul>
+              <li>پشتیبانی آنلاین</li>
+              <li>راهنمای خرید</li>
+              <li>راهنمای استرداد</li>
+              <li>پرسش و پاسخ</li>
+            </ul>
+          </div>
         </div>
-        <div>
-          <p>خدمات مشتریان</p>
-          <ul>
-            <li>پشتیبانی آنلاین</li>
-            <li>راهنمای خرید</li>
-            <li>راهنمای استرداد</li>
-            <li>پرسش و پاسخ</li>
-          </ul>
-        </div>
-        <div>
-          <div>
+        <div className={styles.SecondSectionFooter}>
+          <div className={styles.DivLogoFooter}>
             <Image
               src="/Images/Torino.png"
               alt="logo"
-              width={100}
-              height={100}
+              width={146}
+              height={44}
             />
             <p>تلفن پشتیبانی : 021-8574</p>
           </div>
-          <div>
+          <div className={styles.DivImg}>
             <Image
               src="/Images/aira-682b7c43.png"
               alt="aira"
-              width={100}
-              height={100}
+              width={68}
+              height={74}
             />
             <Image
               src="/Images/samandehi-6e2b448a.png"
               alt="samandehi"
-              width={100}
-              height={100}
+              width={67}
+              height={74}
             />
             <Image
               src="/Images/ecunion-35c3c933.png"
               alt="ecunion"
-              width={100}
-              height={100}
+              width={68}
+              height={74}
             />
             <Image
               src="/Images/passenger-rights-48368f81 1.png"
               alt="passenger-rights"
-              width={100}
-              height={100}
+              width={71}
+              height={74}
             />
             <Image
               src="/Images/home-desktop.png"
               alt="home-desktop"
-              width={100}
-              height={100}
+              width={78}
+              height={74}
             />
           </div>
         </div>
       </div>
-      <div>
+      <div className={styles.DivP}>
         <p>کلیه حقوق این وب سایت متعلق به تورینو میباشد.</p>
       </div>
     </div>
