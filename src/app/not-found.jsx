61acd76c333cd88b43998f0b5c5109f78e7384ee
Mobin Zandkarimi <zandgo2@gmail.com>
2025-07-20 15:58:00 +0330

    make not found page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import Image from "next/image";
+import Link from "next/link";
+import styles from "@/styles/PageNotFound.module.css";
 
 const PageNotFound = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className={styles.pageNotFound}>
+      <div className={styles.errorImage}>
+        <Image
+          src="/Images/Error TV.png"
+          alt="error404"
+          height={555}
+          width={555}
+        />
+      </div>
+      <div className={styles.errorText}>
+        <h3>صفحه مورد نظر یافت نشد!</h3>
+        <Link href="/">بازگشت به صفحه اصلی</Link>
+      </div>
+    </div>
+  );
 };
 
 export default PageNotFound;
