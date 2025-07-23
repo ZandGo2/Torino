0ea3216223262699f9d7a54eed3aa58ddb68bfc7
Mobin Zandkarimi <zandgo2@gmail.com>
2025-07-23 15:01:19 +0330

    make login and verify modal

--- src/components/template/modal/VerifyModal.jsx
+++ src/components/template/modal/VerifyModal.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+"use client"
+import { useState } from "react";
+import OtpInput from "react-otp-input";
 
 const VerifyModal = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    const [otp, setOtp] = useState('');
+  return (
+    <div>
+      <div>
+        <button>--</button>
+      </div>
+      <div>
+        <OtpInput
+          value={otp}
+          onChange={setOtp}
+          numInputs={5}
+          renderSeparator={<span>-</span>}
+          renderInput={(props) => <input {...props} />}
+        />
+        <p>60000</p>
+        <button>send again</button>
+      </div>
+      <button>ورود به تورینو</button>
+    </div>
+  );
 };
 
 export default VerifyModal;
