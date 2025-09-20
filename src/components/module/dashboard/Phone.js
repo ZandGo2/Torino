b6302b01474c4ee85a9656d2292d51da7be9af5f
Mobin Zandkarimi <zandgo2@gmail.com>
2025-09-20 17:56:01 +0330

    phone components

--- src/components/module/dashboard/Phone.js
+++ src/components/module/dashboard/Phone.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import Image from "next/image";
 
 const Phone = () => {
-    return (
+  return (
+    <div>
+      <div>
+        <Image src="/images/Taking.png" alt="" width={308} height={225}/>
         <div>
-            
+            <p>
+                <span>تورینو</span>
+                خرید تلفی از 
+            </p>
+            <p>به هرکجا که میخواهید!</p>
         </div>
-    );
+      </div>
+      <div>
+        <p>
+          <Image
+            src="/images/call1.png"
+            alt="phone icons"
+            width={24}
+            height={24}
+          />
+          <span>021-1840</span>
+        </p>
+        <button>اطلاعات بیشتر</button>
+      </div>
+    </div>
+  );
 };
 
 export default Phone;
