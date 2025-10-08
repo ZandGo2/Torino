519d44012af33932c518e8728d59d0d91d17128f
Mobin Zandkarimi <zandgo2@gmail.com>
2025-10-08 12:29:50 +0330

    make ui for details tours

--- src/components/template/DetailsTour.js
+++ src/components/template/DetailsTour.js
@@ -1,66 +1,106 @@
 import Image from "next/image";
+import { FaMap, FaUserNinja } from "react-icons/fa";
+import { GiStarMedal } from "react-icons/gi";
+import { CiRoute } from "react-icons/ci";
+import { IoCalendar } from "react-icons/io5";
+import { FaBus } from "react-icons/fa";
+import { FaUsersBetweenLines } from "react-icons/fa6";
+import { SiSecurityscorecard } from "react-icons/si";
+import Link from "next/link";
 
-const DetailsTour = ({
-  data: {
+const DetailsTour = ({ data }) => {
+  const {
     price,
     title,
     startDate,
     origin,
-    options,
     image,
     fleetVehicle,
     endDate,
-    destination,
     availableSeats,
-  },
-}) => {
-//   console.log(data);
+  } = data;
+  //   console.log(data);
   return (
-    <div>
-      <div>
-        <div>
+    <div className="w-[100%] bg-gray-50 m-[20px]">
+      <div className="w-[1188px] h-[427px] p-7 bg-white m-auto my-[100px] flex flex-col border border-black/20 rounded-xl">
+        <div className="flex flex-row-reverse ">
           <div>
-            <Image src={image} alt={title} width={397} height={265} />
+            <Image
+              src={image}
+              alt={title}
+              width={397}
+              height={265}
+              className="rounded-xl"
+            />
           </div>
-          <div>
-            <h1>{title}</h1>
-            <p>{}</p>
-            <div>
-              <p>تورلیدر از مبدا</p>
-              <p>برنامه سفر</p>
-              <p>تضمین کیفیت</p>
+          <div className="w-[60%] flex flex-col items-end mr-7">
+            <h1 className="text-3xl font-bold mb-5">{title}</h1>
+            <p className="text-xl text-[#282828] mb-7">5 روز و 4 شب</p>
+            <div className="mb-7 flex flex-row-reverse items-center">
+              <FaUserNinja className="text-[#7D7D7D] ml-3" />
+              <p className="text-xl text-[#7D7D7D] ml-7">تورلیدر از مبدا</p>
+              <FaMap className=" text-[#7D7D7D] ml-3" />
+              <p className="text-xl text-[#7D7D7D] ml-7">برنامه سفر</p>
+              <GiStarMedal className=" text-[#7D7D7D] ml-3" />
+              <p className="text-xl text-[#7D7D7D] ml-7">تضمین کیفیت</p>
             </div>
-            <div>
-              <p>
-                تومان<span>{price}</span>
+            <div className="w-[100%] flex flex-row-reverse justify-between">
+              <p className="text-sm font-normal text-[#282828CC] flex items-center">
+                تومان
+                <span className="text-[#009ECA] text-2xl font-medium ml-3.5">
+                  {price}
+                </span>
               </p>
-              <button>رزرو و خرید</button>
+              <Link
+                href=""
+                className="w-[204px] h-[56px] rounded-xl text-white bg-[#28A745] text-2xl font-normal flex flex-col items-center justify-center"
+              >
+                رزرو و خرید
+              </Link>
             </div>
           </div>
         </div>
-        <div>
-          <div>
-            <p>مبدا</p>
+        <div className="mt-15 w-[100%] flex flex-row-reverse justify-around">
+          <div className="flex flex-col items-center">
+            <div className="flex items-center mb-3.5">
+              <p>مبدا</p>
+              <CiRoute className="text-xl ml-3"/>
+            </div>
             <p>{origin.name}</p>
           </div>
-          <div>
-            <p>تاریخ رفت</p>
-            <p>23 مهر 1403</p>
+          <div className="flex flex-col items-center">
+            <div className="flex items-center mb-3.5">
+              <p>تاریخ رفت</p>
+              <IoCalendar className="text-xl ml-3"/>
+            </div>
+            <p>{startDate}</p>
           </div>
-          <div>
-            <p>تاریخ برگشت</p>
-            <p>28 مهر 1403</p>
+          <div className="flex flex-col items-center">
+            <div className="flex items-center mb-3.5">
+              <p>تاریخ برگشت</p>
+              <IoCalendar className="text-xl ml-3"/>
+            </div>
+            <p>{endDate}</p>
           </div>
-          <div>
-            <p>حمل و نقل</p>
+          <div className="flex flex-col items-center">
+            <div className="flex items-center mb-3.5">
+              <p>حمل و نقل</p>
+              <FaBus className="text-xl ml-3"/>
+            </div>
             <p>{fleetVehicle}</p>
           </div>
-          <div>
-            <p>ظرفیت</p>
+          <div className="flex flex-col items-center">
+            <div className="flex items-center mb-3.5">
+              <p>ظرفیت</p>
+              <FaUsersBetweenLines className="text-xl ml-3"/>
+            </div>
             <p>{availableSeats}</p>
           </div>
-          <div>
-            <p>بیمه</p>
+          <div className="flex flex-col items-center">
+            <div className="flex items-center mb-3.5">
+              <p>بیمه</p>
+              <SiSecurityscorecard className="text-xl ml-3"/>
+            </div>
             <p>بیمه 50 هزار دیناری</p>
           </div>
         </div>
